c9131c45b9f6008ce963fad3309e3f31d20b103a
Tuuga-PC\Tuuga <tuukka.o.muukkonen@gmail.com>
2016-09-21 00:30:01 +0300

    stuff

---
+++ labyrinth-ball-game/Assets/ObstacleState.cs
@@ -0,0 +1,7 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObstacleState : MonoBehaviour {
+	public enum State { cc, c };
+	public State currentState;
+}
---
+++ labyrinth-ball-game/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+	public float force;
+
+	Rigidbody rb;
+
+	void Start () {
+		rb = GetComponent<Rigidbody>();
+	}
+
+	void Update () {
+		var dir = Quaternion.Euler(Vector3.left * 90) * Input.acceleration.normalized;
+		dir.z *= -1;
+		rb.AddForce(dir * force);
+		Debug.DrawRay(transform.position, dir * force, Color.red);
+		Debug.DrawRay(transform.position, rb.velocity, Color.green);
+	}
+}
